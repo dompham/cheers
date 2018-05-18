3b368de41dfd5bf15708ed3f0f2359aba0975219
dompham <do.pham@outlook.com>
2018-05-17 19:44:51 -0700

    Init

---
+++ Cheers/Cheers/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  Cheers
+//
+//  Created by Do Pham on 5/17/18.
+//  Copyright © 2018 Do Pham. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white;
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
