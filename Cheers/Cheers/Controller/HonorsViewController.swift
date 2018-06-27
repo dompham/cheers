6ae918b24dc830e8a6149a0abb2f0a5d502320ea
Do Pham <dpham@digium.com>
2018-06-27 16:52:39 -0700

    Testing camera in honors view

--- Cheers/Cheers/Controller/HonorsViewController.swift
+++ Cheers/Cheers/Controller/HonorsViewController.swift
@@ -8,9 +8,28 @@
 
 import Foundation
 import UIKit
+import ALCameraViewController
 
 class HonorsViewController : UIViewController {
+    var cameraViewController : CameraViewController!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        cameraViewController = CameraViewController { [weak self] image, asset in
+            // Do something with your image here.
+            self?.dismiss(animated: true, completion: {
+                // Return to home
+                self?.tabBarController!.selectedIndex = 0
+            })
+        }
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+
+        logService.log(volume: 2, say: "Presenting Camera")
+        present(cameraViewController, animated: true, completion: nil)
+
     }
 }
